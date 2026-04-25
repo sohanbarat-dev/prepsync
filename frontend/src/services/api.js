47e42d41f7db80a0d7b2a176f8da6b310edd24e8
sohanbarat-dev <sohanbarat2003@gmail.com>
2026-04-25 11:22:48 +0530

    Update frontend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 })
 
-// automatically attach token to every request
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token')
   if (token) {
